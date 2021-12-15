319eec8a6b9ac8b8f930df90dd9f1a4daa21fd6e
viettriit2110 <tridung2721@gmail.com>
2021-12-15 11:35:59 +0700

    update staking

--- Connecus.sol
+++ Connecus.sol
@@ -1,24 +1,30 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity >=0.7.8;
 
 import "./ApetasticERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 
-contract ApetasticERC20Factory is Ownable {
+contract ApetasticERC20Factory is ReentrancyGuard , Context {
     using SafeERC20 for IERC20;
+    IERC20 public immutable token;
 
     address public beneficiary;
     address[] public allTokens;
 
+    mapping (address => bool) _checkStaking;
+
     event TokenCreated(address indexed tokenAddress, uint256 startingSupply);
     event TransferBeneficiary(address indexed oldBeneficiary, address indexed beneficiary);
     event SweepToken(IERC20 indexed token, address indexed beneficiary, uint256 balance);
-
+    // event Approval(address owner, address spender, uint256 value);
     /// @dev On contract creation the beneficiary (and owner) are set to msg.sender
-    constructor() {
+    constructor(address rewardToken_) {
         beneficiary = msg.sender;
+        token = IERC20(rewardToken_);
     }
 
     /// @notice Return the number of tokens created by this contract
@@ -26,33 +32,20 @@ contract ApetasticERC20Factory is Ownable {
         return allTokens.length;
     }
 
-    /// @notice Create a new ERC-20 contract
-    /// @dev Tokens created have 18 decimals which means add 18 zeros the integer supply: 000000000000000000 
-    /// @param name The name of the token
-    /// @param symbol The symbol of the token
-    /// @param supply The totalSupply of the token
     function createToken(string memory name, string memory symbol, uint256 supply) external returns (address) {
-        ApetasticERC20 token = new ApetasticERC20(name, symbol, msg.sender, supply);
-        allTokens.push(address(token));
-        emit TokenCreated(address(token), supply);
-        return address(token);
+        ApetasticERC20 token_ = new ApetasticERC20(name, symbol, msg.sender, supply);
+        allTokens.push(address(token_));
+        emit TokenCreated(address(token_), supply);
+        return address(token_);
     }
+    //0x0230E3760B53b08426d15aaf5A327ED6d0Dd36B7
 
-    /// @notice Change the beneficiary who recieves swept tokens
-    /// @param newBeneficiary The address of the new beneficiary
-    function setBeneficiary(address newBeneficiary) external onlyOwner {
-        address oldBeneficiary = beneficiary;
-        beneficiary = newBeneficiary;
-        emit TransferBeneficiary(oldBeneficiary, beneficiary);
+    function staking(uint256 fee_) external nonReentrant {
+        token.safeTransferFrom(_msgSender(), address(this), fee_ * 10 ** 18);
+        _checkStaking[_msgSender()] = true;
     }
-
-
-    /// @notice An external function to sweep ERC20 tokens locked in this contract.
-    /// @dev Only the owner can call this function, but the tokens are transferred to the beneficiary address
-    /// @param token The address of the ERC20 token to sweep
-    function sweepToken(IERC20 token) external {
-        uint256 balance = token.balanceOf(address(this));
-        token.safeTransfer(beneficiary, balance);
-        emit SweepToken(token, beneficiary, balance);
+    function checkStake() public view returns(bool){
+        return _checkStaking[_msgSender()];
     }
+    
 }
